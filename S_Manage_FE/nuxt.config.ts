48e47ae0ec2a896f7bbc9bbf81cd55013ec67ecf
vit1204 <nguyenthanhviet12042004@gmail.com>
2023-10-12 10:30:25 +0700

    set up basic FE UI

--- S_Manage_FE/nuxt.config.ts
+++ S_Manage_FE/nuxt.config.ts
@@ -2,7 +2,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
+ webpack:{
+   loaders:{
+    vue:{
+       hotReload: true,
+    }
+   }
+ },
 //for tailwind
   css: ['~/index.css'],
   postcss: {
@@ -13,16 +19,47 @@ export default defineNuxtConfig({
   },
 
   //vuetify
- modules:[
+ modules: [
     '@invictus.codes/nuxt-vuetify'
   ],
+  
   vuetify: {
+  
     /* vuetify options */
-    vuetifyOptions: {
+    vuetifyOptions: {                       
+     theme: {
+      themes: {
+        light: {
+          dark:false,
+          colors: {
+            background: '#F5F5F5',
+            primary: '#4A85F6',
+              surface: '#FFFFFF',
+              secondary: '#000000'
+          },
+              
+        },
+        dark:{
+          colors:{
+            background: ''
+          }
+
+        }
+       
+      }
+     }
       // @TODO: list all vuetify options
     },
 
     moduleOptions: {
+      /* nuxt-vuetify module options */
+      treeshaking: false,
+      useIconCDN: true,
+
+      /* vite-plugin-vuetify options */
+      styles: true ,
+      autoImport: true,
+      useVuetifyLabs: true, 
       /* nuxt-vuetify module options */
       
     }
---
+++ S_Manage_FE/pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+    <section class="bg-background flex items-center justify-center h-screen">
+        <div class="w-full max-w-md">
+            <a href="#" class="flex items-center justify-center mb-6 text-2xl font-semibold text-primary">
+                <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
+                S-Manage
+            </a>
+            <div class="bg-surface rounded-lg shadow border-2 border-gray-700">
+                <div class="p-6 space-y-4">
+                    <h1 class=" text-2xl text-center font-bold leading-tight tracking-tight text-gray-900">
+                        Sign in to your account
+                    </h1>
+                    <form class="space-y-4" action="#">
+                        <div class="form-group">
+                            <label for="email" class="block mb-2 text-sm font-medium text-secondary">Username</label>
+                            <input type="email" name="email" id="email"
+                                class="w-full p-2.5 rounded-lg focus:ring-primary-600 focus:border-primary-600 text-secondary  "
+                                placeholder="Username" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="password" class="block mb-2 text-sm font-medium text-secondary">Password</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••"
+                                class="w-full p-2.5 rounded-lg focus:ring-primary-600 focus:border-primary-600 "
+                                required>
+                        </div>
+                        <div class="flex items-center justify-between">
+                            <div class="flex items-start">
+                                <div class="flex items-center h-5">
+                                    <input id="remember" aria-describedby="remember" type="checkbox"
+                                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                                        required>
+                                </div>
+                                <div class="ml-3 text-sm">
+                                    <label for="remember" class="text-secondary">Remember me</label>
+                                </div>
+                            </div>
+                            <a href="#"
+                                class="text-sm font-medium text-primary hover:underline dark:text-primary-500"><NuxtLink to="/resetPass">Forgot password</NuxtLink></a>
+                        </div>
+                        <v-btn block variant="outlined" size="x-large" class="bg-primary mb-4"> Sign In </v-btn>
+                        <p class="text-sm font-light text-secondary">
+                            Don’t have an account yet? <a href="#"
+                                class="font-medium text-primary hover:underline dark:text-primary-500"><NuxtLink to="/Register">Sign up
+                                        </NuxtLink></a>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+.form-group {
+    margin-bottom: 1rem;
+}
+
+label {
+    margin-bottom: 0.5rem;
+}
+
+input {
+    padding: 0.5rem;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}</style>
