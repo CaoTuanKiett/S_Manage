16b09be15e6eb572e24a11c86f3b0a53206a7800
duypro432003 <vanduyluu432003@gmail.com>
2023-10-13 23:37:09 +0700

    update payment with stripe

--- S_Manage_BE/src/app.js
+++ S_Manage_BE/src/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const knex = require('../knexfile')
 const app = express();
-
-
-require('dotenv').config();
-
-const port = process.env.PORT ;
-
+const port = 9696;
+const stripe = require('./routers/stripe/stripe')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/payment', stripe);
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
--- S_Manage_BE/src/routers/stripe/stripe.js
+++ S_Manage_BE/src/routers/stripe/stripe.js
@@ -2,8 +2,9 @@ const stripe = require('stripe');
 const env = require('dotenv');
 env.config();
 const express = require('express');
+const db=require('../../database/connectDB')
 const stripeRouter = express.Router();
-
+// api tạo due
 const stripeInstance = stripe(process.env.STRIPE_SECRET_KEY);
 stripeRouter.post('/stripe-checkout', async (req, res) => {
     const session = await stripeInstance.checkout.sessions.create({
@@ -16,14 +17,19 @@ stripeRouter.post('/stripe-checkout', async (req, res) => {
                 price_data: {
                     currency: "vnd",
                     product_data: {
-                        name: item.name,
-                        description: item.shortDes,
+                        name: "Tiền Tháng " + item.month, /// tháng mấy 
+                        description: req.body.description /// mô tả
+
                     },
-                    unit_amount: item.price
+                    unit_amount: item.price? item.price : 200000 // số tiền mỗi tháng
                 },
-                quantity: item.quantity
+                quantity: 1 
             }
-        })
+        }),
+        metadata: {
+            user_id: req.body.user_id,
+            description: req.body.description
+        }
     })
     res.json(session.url)
 });
@@ -31,12 +37,28 @@ stripeRouter.get('/pay-success', async (req, res) => {
     const session_id = req.query.session_id;
     try {
         const session = await stripeInstance.checkout.sessions.retrieve(session_id);
-        const customer = session.customer_details
-        res.json(customer)
+        // const paymentIntentId = session.payment_intent
+        // const paymentIntent = await stripeInstance.paymentIntents.retrieve(paymentIntentId);
+        // const customer = session.customer_details;
+        //res.json(session)
+        try {
+            await db('payment').insert({
+                amount_money: session.amount_total,
+                user_id: session.metadata.user_id,
+                account_name:session.customer_details.name,
+                description: session.metadata.description,
+                pay_method: session.payment_method_types,
+                create_at: new Date(session.created * 1000).toISOString().slice(0, 19).replace('T', ' ')
+            });
+
+            res.json("save payment success");
+        } catch (error) {
+            res.json("can't save the payment. Error message: " + error);
+        }
     } catch (error) {
-        res.json("bug")
+        res.json("error");
     }
-})
+});
 
 module.exports = stripeRouter;
 
