ab4dd55a1ae31248eb53b27ffcda9b13d681a139
CaoTuanKiett <tuankiet06032002@gmail.com>
2023-10-21 09:04:04 +0700

    '#2_update_database'

--- S_Manage_BE/src/database/migrations/20231012025511_create_payment_table.js
+++ S_Manage_BE/src/database/migrations/20231012025511_create_payment_table.js
@@ -10,9 +10,9 @@ exports.up = function(knex) {
     table.string('pay_method').notNullable();
     table.dateTime('create_at').notNullable();
     table.string('account_name');
-    table.string('credit_card_first');
-    table.string('credit_card_last');
     table.string('account_number').notNullable();
+    table.string('account_money');
+    table.string('description');
 
     // Tạo foreign key constraint để liên kết user_id với bảng "user"
     table
