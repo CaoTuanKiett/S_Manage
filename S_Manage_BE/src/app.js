e52d146db3c36c39bbe65913429e4ccb76205f37
CaoTuanKiett <tuankiet06032002@gmail.com>
2023-10-29 22:22:17 +0700

    '#1_upup'

--- S_Manage_BE/src/app.js
+++ S_Manage_BE/src/app.js
@@ -17,6 +17,13 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(`${process.env.API_V1}/payment`, stripe);
